02389ddfa93473b4ea3df40c5428bf762724aec6
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-12-17 01:11:52 +0700

    '9.3.4_formRegister'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Navigation/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
           <Route path="/contact">contact</Route>
           <Route path="/login">
             <Login />
+          </Route>
+          <Route path="/register">
+          <Register/>
           </Route>
           <Route path="/" exact>
             Home
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,12 @@
 import "./Login.scss";
+import {useHistory} from "react-router-dom"
+
 const Login = (props) => {
+  let history = useHistory()
+
+  const handleCreateNewAccount = () => {
+    history.push("/register")
+  }
   return (
     <div className="login-container mt-3">
       <div className="container">
@@ -29,7 +36,9 @@ const Login = (props) => {
                 </span>
               <hr />
               <div className="text-center">
-                <button className="btn btn-success">Create new account</button>
+                <button className="btn btn-success" onClick={() => handleCreateNewAccount()}>
+                  Create new account
+                  </button>
               </div>
           </div>
         </div>
