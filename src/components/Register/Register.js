0ee122be04f4ad80f43425a4dae371baba695eaf
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-12-23 23:00:52 +0700

    '10.2.2_registerCompleted'

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,6 +3,7 @@ import { useHistory } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
+import { registerNewUser } from "../../services/userService";
 
 const Register = (props) => {
   const [email, setEmail] = useState("");
@@ -63,18 +64,21 @@ const Register = (props) => {
     return true;
   };
 
-  const handleRegister = () => {
+  const handleRegister = async () => {
     let check = isValidInput();
 
     if (check === true) {
-      axios.post("http://localhost:8080/api/v1/register", {
-        email,
-        phone,
-        username,
-        password,
-      });
+      let response = await registerNewUser(email, phone, username, password);
+      let serverData = response.data;
+      if(+serverData.EC === 0) {
+        toast.success(serverData.EM)
+        history.push("/login")
+      } else {
+        toast.error(serverData.EM)
+      }
     }
   };
+
   return (
     <div className="login-container mt-3">
       <div className="container">
