5dc2e922a7acdbf133c7e4835c8bfd95ed88a68c
Nguyen Duy Loi <nguyenduyloi04@gmail.com>
2025-12-23 23:34:07 +0700

    '10.3.1_stateLoginForm'

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,40 @@
+import { useState } from "react";
 import "./Login.scss";
-import {useHistory} from "react-router-dom"
-
+import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
+import { loginUser } from "../../services/userService";
 const Login = (props) => {
-  let history = useHistory()
+  let history = useHistory();
+
+  const [valueLogin, setValueLogin] = useState("");
+  const [password, setPassword] = useState("");
+
+  const defaultObjValidInput = {
+    isValidValueLogin: true,
+    isValidPassword: true,
+  };
+  const [objValidInput, setObjValidInput] = useState(defaultObjValidInput);
 
   const handleCreateNewAccount = () => {
-    history.push("/register")
-  }
+    history.push("/register");
+  };
+
+  const handleLogin = async () => {
+    setObjValidInput(defaultObjValidInput);
+    if (!valueLogin) {
+      setObjValidInput({ ...defaultObjValidInput, isValidValueLogin: false });
+      toast.error("Please enter your email address or phone number.");
+      return;
+    }
+
+    if (!password) {
+      setObjValidInput({ ...defaultObjValidInput, isValidPassword: false });
+      toast.error("Please enter your password.");
+      return;
+    }
+
+    await loginUser(valueLogin, password);
+  };
   return (
     <div className="login-container mt-3">
       <div className="container">
@@ -18,28 +46,51 @@ const Login = (props) => {
               life.
             </div>
           </div>
-            <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
-              <div className="brand d-sm-none">Hoi Dan IT</div>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Email address or phone number"
-              />
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-              <button className="btn-primary">Login</button>
-              <span className="text-center">
-                <a className="forgot-password" href="#">Forgot your password?</a>
-                </span>
-              <hr />
-              <div className="text-center">
-                <button className="btn btn-success" onClick={() => handleCreateNewAccount()}>
-                  Create new account
-                  </button>
-              </div>
+          <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3">
+            <div className="brand d-sm-none">Hoi Dan IT</div>
+            <input
+              type="text"
+              className={
+                objValidInput.isValidValueLogin
+                  ? "form-control"
+                  : "is-invalid form-control"
+              }
+              placeholder="Email address or phone number"
+              value={valueLogin}
+              onChange={(event) => {
+                setValueLogin(event.target.value);
+              }}
+            />
+            <input
+              type="password"
+              className={
+                objValidInput.isValidPassword
+                  ? "form-control"
+                  : "is-invalid form-control"
+              }
+              placeholder="Password"
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            />
+            <button className="btn-primary" onClick={() => handleLogin()}>
+              Login
+            </button>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Forgot your password?
+              </a>
+            </span>
+            <hr />
+            <div className="text-center">
+              <button
+                className="btn btn-success"
+                onClick={() => handleCreateNewAccount()}
+              >
+                Create new account
+              </button>
+            </div>
           </div>
         </div>
       </div>
